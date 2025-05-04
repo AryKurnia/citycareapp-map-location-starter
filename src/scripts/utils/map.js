cce9221838b3c15951a8119d4f2a4d828a7bb1ee
AryKurnia <aryk922@gmail.com>
2025-05-04 20:06:33 +0800

    Add map in buat laporan fitur

--- src/scripts/utils/map.js
+++ src/scripts/utils/map.js
@@ -83,6 +83,14 @@ export default class Map {
         this.#map.setView(latLng(coordinate), zoomLevel);
     }
 
+    getCenter() {
+        const { lat, lng } = this.#map.getCenter();
+        return {
+            latitude: lat,
+            longitude: lng,
+        };
+    }
+
     createIcon(options = {}) {
         return icon({
             ...Icon.Default.prototype.options,
@@ -112,5 +120,9 @@ export default class Map {
         }
         newMarker.addTo(this.#map);
         return newMarker;
+    }
+
+    addMapEventListener(eventName, callback) {
+        this.#map.addEventListener(eventName, callback);
     }
 }
